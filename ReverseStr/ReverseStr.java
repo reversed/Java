b645c3f1dc8536d0d3eb63f5cb3bd1c72b89d5b6
unknown <lechen1@SHA-L7-LECHEN1.amd.com>
2016-07-27 12:58:23 +0800

    add ReverseStr code snippet

---
+++ ReverseStr/ReverseStr.java
@@ -0,0 +1,45 @@
+import java.io.*;
+
+public class ReverseStr
+{
+	
+	public static String ReverseString(String str)
+	{
+		char[] arr = str.toCharArray();
+		
+		int start = 0;
+		int end = str.length() - 1;
+		while (start <= end)
+		{
+			char temp = arr[end];
+			arr[end] = arr[start];
+			arr[start] = temp;
+			++start;
+			--end;
+		}
+		
+		return str.valueOf(arr);
+	}
+	
+	public static void main(String[] arg)
+	{
+		String str = "";
+		
+		BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("Enter a string: ");
+		try
+		{
+			str = stdin.readLine();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		
+		String rev = ReverseString(str);
+		System.out.print("Reversed string: " + rev);
+	}
+}
+
+
+
